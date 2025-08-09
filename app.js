7eedbd78650b7730e83d83081a1a7ffcff1bd66f
branjit28 <99332587+branjit28@users.noreply.github.com>
2025-08-09 21:33:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD Pipeline!');
+  res.send('Hello Thayapathi from CI/CD Pipeline!');
 });
 
 app.listen(port, () => {
